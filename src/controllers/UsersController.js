e2ffe432c28aea6b99731863cd1ad34cb37abf33
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-10-12 11:42:17 -0300

    update and create usersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,17 +1,68 @@
+const { hash, compare } = require("bcryptjs");
+
 const AppError = require("../utils/AppError");
+const knex = require("../database/knex");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError("O nome é obrigatório");
+    const checkUserExists = await knex("users").where({ email }).first();
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail ja está em uso", 401);
     }
 
-    response.json({name, email, password});
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users").insert({ name, email, password: hashedPassword });
+
+    return response.status(201).json();
   }
 
-  update() {}
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const user = await knex("users").where({ id }).first();
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado", 401);
+    }
+
+    const userWithUpdatedEmail = await knex("users").where({ email }).first();
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail ja está em uso", 401);
+    } 
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha", 401);
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga está incorreta", 401);
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await knex("users").where({ id }).update({
+      name: user.name,
+      email: user.email,
+      password: user.password
+    });
+
+    await knex("users").where({ id }).update('updated_at', knex.fn.now());
+
+    return response.status(200).json();
+  }
 }
 
 module.exports = UsersController;
