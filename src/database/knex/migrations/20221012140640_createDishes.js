8ede525ff4539ee3b72fae855b755441d9a3a116
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-10-12 11:27:52 -0300

    created seeds and fixed error timestamp
    fixed error timestamp

--- src/database/knex/migrations/20221012140640_createDishes.js
+++ src/database/knex/migrations/20221012140640_createDishes.js
@@ -7,8 +7,8 @@ exports.up = knex => knex.schema.createTable("dishes", table => {
   table.text("price");
   table.integer("amount");
 
-  table.timestamps("created_at").default(knex.fn.now());
-  table.timestamps("updated_at").default(knex.fn.now());
+  table.timestamp("created_at").default(knex.fn.now());
+  table.timestamp("updated_at").default(knex.fn.now());
 });
 
 exports.down = knex => knex.schema.dropTable("dishes");
