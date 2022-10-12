dee019fdc774fbdc88f6c329dcfb0562e6b16739
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-10-12 14:33:29 -0300

    created SessionsController and added JWT

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
 const dishesRouter = require("./dishes.routes");
+const sessionsRoutes = require("./sessions.routes");
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
 routes.use("/dishes", dishesRouter)
+routes.use("/sessions", sessionsRoutes)
 
 module.exports = routes;
