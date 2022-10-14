34788cca64c12ad27739e4869efa862fb0cc8355
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-10-14 16:43:13 -0300

    refactoring createDishes

--- src/database/knex/migrations/20221012140640_createDishes.js
+++ src/database/knex/migrations/20221012140640_createDishes.js
@@ -5,7 +5,6 @@ exports.up = knex => knex.schema.createTable("dishes", table => {
   table.text("category");
   table.text("image");
   table.text("price");
-  table.integer("amount");
 
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
